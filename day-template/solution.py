322bb30237676a5c2eb7119f68423e67e47cc87b
Raph Christin <70120121+raphaelchristin@users.noreply.github.com>
2023-12-04 16:12:19 -0800

    reorganize template

--- day-template/solution.py
+++ day-template/solution.py
@@ -1,11 +1,10 @@
-from solve import *
-
-
 def main():
     with open("input.txt", "r") as input:
         lines = input.readlines()
-    return 0
+    return 0, 0
 
 
 if __name__ == "__main__":
-    print(f"The result for part 1 is: {main()}")
+    part1, part2 = main()
+    print(f"The result for part 1 is: {part1}")
+    print(f"The result for part 2 is: {part2}")
